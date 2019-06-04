5922f895681e9c25547a4c954d35be090f240951
KnoblauchPC <maxime.rigaud.perso@gmail.com>
2019-06-04 22:31:31 +0200

    Added some base implementation for 'TabWidget' but nothing functional yet

--- src/TabWidget.cc
+++ src/TabWidget.cc
@@ -1,16 +1,119 @@
 
 # include "TabWidget.hh"
+# include "SelectorWidget.hh"
 
 namespace sdl {
   namespace graphic {
 
     TabWidget::TabWidget(const std::string& name,
                          core::SdlWidget* parent,
+                         const TabPosition& tabLayout,
                          const utils::Sizef& area):
-      core::SdlWidget(name, area, parent)
-    {}
+      core::SdlWidget(name, area, parent),
+      m_activeTab(-1),
+      m_tabLayout(tabLayout),
+      m_titlesLayout(nullptr),
+      m_tabs()
+    {
+      build();
+    }
 
     TabWidget::~TabWidget() {}
+
+    void
+    TabWidget::insertTab(const int /*index*/,
+                         core::SdlWidget* item,
+                         const std::string& /*text*/)
+    {
+      // Check trivial case where the item is not valid.
+      if (item == nullptr) {
+        error(
+          std::string("Cannot insert null tab in tabwidget"),
+          std::string("Invalid null tab")
+        );
+      }
+
+      // In order to correctly insert the item into the tabwidget we need
+      // to both insert its representation into the selector layout but
+      // also to insert the corresponding title in the bar.
+      // TODO: Perform insertion of the newly created label widget ?
+
+      // First insert the widget into the selector layout.
+      // TODO: Perform insertion of the item.
+      // TODO: Perform check for LinearLayout removal.
+    }
+
+    void
+    TabWidget::removeTab(const int index) {
+      // Try to find the name associated whith the input `index`.
+      const TabsMap::const_iterator tab = m_tabs.find(index);
+      if (tab == m_tabs.cend()) {
+        error(
+          std::string("Cannot remove item ") + std::to_string(index) + " from tabwidget",
+          std::string("No such item")
+        );
+      }
+
+      // In order to remove this item we need to do two things:
+      // 1. remove the item from the children of this widget.
+      // 2. remove the title associated to this widget from the
+      //    titles bar.
+
+      // TODO: Handle removal of the tab.
+    }
+
+    void
+    TabWidget::build() {
+      // This item is implemented in terms of a selector layout
+      // which is associated to a linear layout where items are
+      // representing the names of the widgets inserted in this
+      // component.
+
+      // This item is managed by a vertical linear layout which
+      // should ever contain two items:
+      // 1. the selector layout representing the content of the
+      //    widgets inserted in this component.
+      // 2. another linear layout representing the titles of the
+      //    tab widgets inserted.
+      //
+      // We choose not to create an additional widget to support
+      // this layout and rather use the hierarchical nesting of
+      // the layout items.
+
+      LinearLayoutShPtr layout = std::make_shared<LinearLayout>(
+        std::string("tabwidget_layout"),
+        this,
+        LinearLayout::Direction::Vertical,
+        0.0f,
+        1.0f
+      );
+
+      // Create the secondary layout which will handle positionning
+      // of widgets' titles.
+      m_titlesLayout = std::make_shared<LinearLayout>(
+        std::string("tabwidget_titles_layout"),
+        nullptr,
+        LinearLayout::Direction::Horizontal,
+        0.0f,
+        1.0f
+      );
+
+      // Create the selector layout which will contain the various
+      // widgets inserted into this component.
+      SelectorWidget* selector = new SelectorWidget(
+        std::string("tabwidget_selector"),
+        this,
+        false
+      );
+
+      // Add the nested titles layout and the selector widget to
+      // the general layout for this item.
+      layout->addItem(m_titlesLayout.get());
+      layout->addItem(selector);
+
+      // Assign the layout to this widget.
+      setLayout(layout);
+    }
 
   }
 }
--- src/TabWidget.hh
+++ src/TabWidget.hh
@@ -2,8 +2,9 @@
 # define   TAB_WIDGET_HH
 
 # include <memory>
-# include <vector>
+# include <unordered_map>
 # include <sdl_core/SdlWidget.hh>
+# include "LinearLayout.hh"
 
 namespace sdl {
   namespace graphic {
@@ -11,11 +12,82 @@ namespace sdl {
     class TabWidget: public core::SdlWidget {
       public:
 
+        /**
+         * @brief - Describes the position of the tab indication in this widget relatively
+         *          to the content of the widgets.
+         */
+        enum class TabPosition { 
+          North, //<! - Tab indication will be displayed above the content.
+          South, //<! - Tab indication will be displayed below the content.
+          West,  //<! - Tab indication will be displayed on the left of the content.
+          East   //<! - Tab indication will be displayed on the right of the content.
+        };
+
+      public:
+
         TabWidget(const std::string& name,
                  core::SdlWidget* parent = nullptr,
+                 const TabPosition& tabLayout = TabPosition::North,
                  const utils::Sizef& area = utils::Sizef());
 
         virtual ~TabWidget();
+
+        /**
+         * @brief - Used to insert the specified widget at the index `index` under the
+         *          specified title. We use the index as a loose description of the
+         *          desired position of the widget and value like `-1` or `size + 1`
+         *          are accepted and will result respectively in the tab being inserted
+         *          before the first tab or after the last one.
+         *          Note that if the user does not provide any `text` for this tab widget
+         *          to be referenced, the name of the item will be used instead.
+         * @param index - the position where the tab widget should be inserted.
+         * @param item - a pointer providing the description of the widget to insert.
+         * @param text - a string representing the name under which the item should be
+         *               referenced in the title bar. If no name is provided the widget's
+         *               name will be used.
+         */
+        void
+        insertTab(const int index,
+                  core::SdlWidget* item,
+                  const std::string& text = std::string());
+
+        /**
+         * @brief - Used to remove the tab widget located at index `index`. If no such
+         *          index exists in this component an error is raised.
+         * @param index - the position of the tab widget to remove from this layout.
+         */
+        void
+        removeTab(const int index);
+
+      private:
+
+        /**
+         * @brief - Used to build this component by creating the adequate layout and the component
+         *          to use to represent each item of the tab widget.
+         */
+        void
+        build();
+
+      private:
+
+        using TabsMap = std::unordered_map<int, std::string>;
+
+        int m_activeTab;
+        TabPosition m_tabLayout;
+
+        /**
+         * @brief - Contains the description of the layout positionning titles of items inserted in
+         *          this tab widget. This basically corresponds to the way we allow the user to
+         *          switch from one tab to another.
+         */
+        LinearLayoutShPtr m_titlesLayout;
+
+        /**
+         * @brief - Associates the index of the tab with its name. This allows the user to reference
+         *          tabs by index instead of using their name (which might not have been provided by
+         *          the user).
+         */
+        TabsMap m_tabs;
 
     };
 
