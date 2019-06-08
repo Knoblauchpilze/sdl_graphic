8a9cc403de663f3fb7c611b1b6a6af43e08f8a97
KnoblauchPC <maxime.rigaud.perso@gmail.com>
2019-06-08 17:20:26 +0200

    Removed automatic insertion of widgets parented with a 'SelectorLayout' into the corresponding layout: one has to manually call 'insertWidget' to perform the register operation in the layout

--- src/SelectorWidget.cc
+++ src/SelectorWidget.cc
@@ -23,30 +23,5 @@ namespace sdl {
       getLayout().addItem(widget, index);
     }
 
-    void
-    SelectorWidget::addWidget(core::SdlWidget* widget) {
-      // In order not to break the encapsulation and keep the fact that this
-      // item uses a `SelectorLayout`, we prefer to reimplement this method so
-      // that it is still possible to keep the standard way of parenting
-      // widgets: add this item as parent when creating the children.
-      // Of course we still need to perform the base operations.
-      // Also note that this method does not allow to choose the order of the
-      // inserted widget: it will be inserted at the last position of the
-      // layout.
-
-      core::SdlWidget::addWidget(widget);
-
-      // Add the input `widget` to the layout.
-      SelectorLayout* layout = getLayoutAs<SelectorLayout>();
-      if (layout == nullptr) {
-        error(
-          std::string("Could not insert widget \"") + widget->getName() + "\" into selector widget",
-          std::string("Invalid layout")
-        );
-      }
-
-      layout->addItem(widget);
-    }
-
   }
 }
