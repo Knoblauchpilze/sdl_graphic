f0faa30723d3c6906bf764c69c44406ef3d12c45
KnoblauchPC <maxime.rigaud.perso@gmail.com>
2019-07-06 23:17:18 +0200

    Handled area update in case of 'PictureWidget' in 'Crop' mode

--- src/PictureWidget.cc
+++ src/PictureWidget.cc
@@ -53,39 +53,56 @@ namespace sdl {
         // We need to convert the input area to a format which can be used by
         // the engine.
         utils::Boxf converted = convertToEngineFormat(area);
-        log("Drawing picture while updating region " + area.toString() + " (converted: " + converted.toString());
+        log("Drawing picture while updating region " + area.toString() + " (converted: " + converted.toString() + ")");
 
-        // TODO: We should care about updating only part of the texture: indeed imagine that the
-        // root widget is a picture widget: if a child get updated the folliwing call will draw
-        // the entire texture behind the widget which is not what we want. We want to copy only
-        // the part of the picture which would effectively be blitted at this position.
-        getEngine().drawTexture(m_picture, &uuid, &converted);
+        // TODO: We should care about updating only part of the texture: indeed
+        // imagine that the root widget is a picture widget: if a child get updated
+        // the folliwing call will draw the entire texture behind the widget which
+        // is not what we want. We want to copy only the part of the picture which
+        // would effectively be blitted at this position.
+        getEngine().drawTexture(m_picture, nullptr, &uuid, &converted);
       }
 
       // Handle `Crop` mode.
       if (m_mode == Mode::Crop) {
+        // In crop mode, the final area corresponds to a position where the image
+        // will be centered, and the dimensions are consistent with the internal
+        // image dimensions. Due to how we use the position of textures, the final
+        // rect is always set to `[0; 0]` because the picture is centered in the
+        // parent widget.
+        // The convert method will set it to the middle of the widget as expected
+        // by the engine.
+        //
+        // In order to crop the picture, we need to select the area of the picture
+        // which corresponds to its center. This center will be displayed on all
+        // the available space.
+        // We can note that either the picture is smaller than the available space
+        // in which case the image will be centered, or the picture is larger than
+        // the available space, in which case the center of the image will be
+        // displayed.
+        // Two steps are required to determine which part of the image should be
+        // drawn in the input `area`:
+        // 1. The part of the image which should be displayed needs to be computed.
+        // 2. The actual blit part needs to be computed both from the part of the
+        //    image displayed and the area to update.
+
+        // First, proceed to the determination of the part of the image which is
+        // drawn: in a first approach, the whole image can be displayed.
         utils::Sizei sizePic = getEngine().queryTexture(m_picture);
+        utils::Boxf srcRect = utils::Boxf::fromSize(sizePic, true);
+
+        // Now we actually need to restrain the area to the available space.
         utils::Sizei sizeEnv = getEngine().queryTexture(uuid);
+        utils::Boxf dstRect = utils::Boxf::fromSize(sizeEnv, true);
 
-        utils::Boxf dstRect(
-          (sizeEnv.w() - sizePic.w()) / 2.0f,
-          (sizeEnv.h() - sizePic.h()) / 2.0f,
-          sizePic.w(),
-          sizePic.h()
-        );
+        srcRect = srcRect.intersect(dstRect);
+        utils::Boxf srcRectEngine = utils::Boxf(sizePic.w() / 2.0f, sizePic.h() / 2.0f, srcRect.w(), srcRect.h());
 
-        // TODO: Handle `area`.
-        getEngine().drawTexture(m_picture, &uuid, &dstRect);
-      }
+        // Now determine the blit coordinates by converting the `dstRect`.
+        utils::Boxf dstRectEngine = convertToEngineFormat(dstRect);
 
-      // Compute the blit position of the picture so that it is centered.
-      if (m_picture.valid()) {
-        // Perform the copy operation according to the display mode.
-        if (m_mode == Mode::Crop) {
-        }
-
-        if (m_mode == Mode::Fit) {
-        }
+        // Draw the texture with specified rects.
+        getEngine().drawTexture(m_picture, &srcRectEngine, &uuid, &dstRectEngine);
       }
     }
 
