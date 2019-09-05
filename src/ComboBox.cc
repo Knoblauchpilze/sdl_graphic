ab93f33d328a75a6e24bc25fd5894802b0a5401c
KnoblauchPC <maxime.rigaud.perso@gmail.com>
2019-09-05 21:30:36 +0200

    Updated 'ComboBox' and 'SelectorWidget' with latest interface of 'FocusEvent'

--- src/ComboBox.cc
+++ src/ComboBox.cc
@@ -210,7 +210,7 @@ namespace sdl {
 
             // Also, deactivate the corresponding child so that we don't
             // keep it selected.
-            core::engine::FocusEventShPtr foe = core::engine::FocusEvent::createFocusOutEvent(e.getReason(), e.getEmitter());
+            core::engine::FocusEventShPtr foe = core::engine::FocusEvent::createFocusOutEvent(e.getReason(), false, e.getEmitter());
             postEvent(foe, false, true);
           }
         }
