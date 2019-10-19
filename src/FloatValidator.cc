8a2375e5fa85954f93ac50bfbec39e05ab012be3
KnoblauchPC <maxime.rigaud.perso@gmail.com>
2019-10-19 18:11:14 +0200

    Started implementation of 'FloatValidator' by providing a similar interface to the 'IntValidator'

--- src/FloatValidator.cc
+++ src/FloatValidator.cc
@@ -2,46 +2,71 @@
 # include "FloatValidator.hh"
 # include <string>
 
+namespace {
+
+  /**
+   * @brief - Used to attempt to convert the input string to a valid float value.
+   *          Note that if the input string cannot be converted the returned value
+   *          will be `0.0` and the `ok` boolean will be set to `false` if it is
+   *          not set to `nullptr`.
+   * @param input - the string to convert to a float.
+   * @param ok - a pointer which should be set if the user wants to know whether the
+   *             input string could be successfully converted to a float value.
+   * @return - the float represented by the input string or `0.0` if the string is
+   *           not a valid number.
+   */
+  float
+  convertToFloat(const std::string& input,
+                 bool* ok = nullptr) noexcept
+  {
+    // Use the dedicated conversion function.
+    size_t end;
+    float val;
+    bool valid = true;
+
+    try {
+      val = std::stof(input.c_str(), &end);
+    }
+    catch (const std::invalid_argument& e) {
+      // No conversion could be performed.
+      valid = false;
+    }
+    catch (const std::out_of_range& e) {
+      // The value seems to be valid but cannot be represented using a float value.
+      valid = false;
+    }
+
+    // Analyze the result of the conversion.
+    if (ok != nullptr) {
+      *ok = valid && (end >= input.size());
+    }
+
+    // Assign a `0` value in case the conversion failed.
+    if (!valid || end < input.size()) {
+      val = 0;
+    }
+
+    // Return the converted value.
+    return val;
+  }
+
+}
+
 namespace sdl {
   namespace graphic {
 
     Validator::State
     FloatValidator::validate(const std::string& input) const {
-      // Check whether we can cast the input string to a valid integer.
-      size_t end;
+      // Try to convert the input string to an integer value: if this cannot be done
+      // we have a trivial case of an invalid input.
+      bool ok = false;
+      float val = convertToFloat(input, &ok);
 
-      float val;
-      bool valid = true;
-      try {
-        val = std::stof(input.c_str(), &end);
-      }
-      catch (const std::invalid_argument& e) {
-        // No conversion could be performed.
-        valid = false;
-      }
-      catch (const std::out_of_range& e) {
-        log(
-          std::string("Value \"") + input + "\" seems to be a valid float but can't be represented",
-          utils::Level::Warning
-        );
-        valid = false;
-      }
-
-      // If the conversion could not occur, the input string is considered not valid.
-      if (!valid) {
-        return State::Invalid;
-      }
-
-      // Also check whether there is nothing after the number in the input string.
-      if (end <= input.size()) {
-        return State::Invalid;
-      }
-
-      if (end == '\0') {
-        log("Successfully converted \"" + input + "\" to " + std::to_string(val));
-      }
-      else {
-        log("Could not convert \"" + input + "\" to a number");
+      // Trivial case of the empty string which allows to safely access at least the
+      // first characters in later tests.
+      if (input.empty()) {
+        // Consider empty string as intermediate.
+        return State::Intermediate;
       }
 
       // Now check the value of the float against the range: we can either have
